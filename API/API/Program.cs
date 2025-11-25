b46a964cdd748810992d71d90b31b679091f1601
GUILHERME GARCIA DOS SANTOS <guilherme.santos05@cs.up.edu.br>
2025-11-24 21:05:03 -0300

    back bem feito

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -5,12 +5,20 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>();
 
+builder.Services.AddCors(options =>
+    options.AddPolicy("Acesso Total",
+        configs => configs
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod())
+);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "COLOQUE O SEU NOME");
+app.MapGet("/", () => "Guilherme G");
 
 //ENDPOINTS DE TAREFA
-//GET: http://localhost:5273/api/chamado/listar
+//GET: http://localhost:5000/api/chamado/listar
 app.MapGet("/api/chamado/listar", ([FromServices] AppDataContext ctx) =>
 {
     if (ctx.Chamados.Any())
@@ -20,30 +28,51 @@ app.MapGet("/api/chamado/listar", ([FromServices] AppDataContext ctx) =>
     return Results.NotFound("Nenhum chamado encontrada");
 });
 
-//POST: http://localhost:5273/api/chamado/cadastrar
+//POST: http://localhost:5000/api/chamado/cadastrar
 app.MapPost("/api/chamado/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Chamado chamado) =>
 {
+    if(chamado == null)
+    {
+        return Results.NotFound("Chamado não encontrado");
+    }
     ctx.Chamados.Add(chamado);
     ctx.SaveChanges();
     return Results.Created("", chamado);
 });
 
-//PUT: http://localhost:5273/chamado/alterar/{id}
+//PUT: http://localhost:5000/chamado/alterar/{id}
 app.MapPut("/api/chamado/alterar/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
 {
-    //Implementar a alteração do status do chamado
+            Chamado? chamado = ctx.Chamados.Find(id);
+
+     if(chamado is null)
+        {
+            return Results.NotFound("Chamado não encontrado");
+        }
+        if(chamado.Status == "Aberto")
+        {
+            chamado.Status = "Em Atendimento";
+        }else if (chamado.Status == "Em Atendimento")
+        {
+            chamado.Status = "Resolvidos";
+        }
+        ctx.Chamados.Update(chamado);
+        ctx.SaveChanges();
+        return Results.Ok(ctx.Chamados.ToList());
+
 });
 
-//GET: http://localhost:5273/chamado/naoconcluidas
+//GET: http://localhost:5000/chamado/naoresolvidos
 app.MapGet("/api/chamado/naoresolvidos", ([FromServices] AppDataContext ctx) =>
 {
-    //Implementar a listagem dos chamados não resolvidos
+    return Results.Ok(ctx.Chamados.Where(x => x.Status != "Resolvido").ToList());
 });
 
-//GET: http://localhost:5273/chamado/concluidas
+//GET: http://localhost:5000/chamado/resolvidos
 app.MapGet("/api/chamado/resolvidos", ([FromServices] AppDataContext ctx) =>
 {
-    //Implementar a listagem dos chamados resolvidos
+    return Results.Ok(ctx.Chamados.Where(x => x.Status == "Resolvido").ToList());
 });
 
+app.UseCors("Acesso Total");
 app.Run();
